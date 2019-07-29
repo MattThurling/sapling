6fb2ad68eeb9d6c965f78fe2ba1e0a10cc211a56
Matt Thurling <matt@cpdfoteachers.com>
2019-07-29 09:47:52 +0100

    Links from Home

---
+++ static/js/scan.js
@@ -0,0 +1,12 @@
+
+// https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx
+BarcodeReader.licenseKey = 't0068NQAAAA9xqcBsrb1DuHpiuyXSc3C7EdWbaxEagkUKVVNMdGTtUkeW4rNjxGV0DkaMn5OldSQJsnhkVKrT1os5WZDlvIU=';
+let scanner = new BarcodeReader.Scanner({
+    onFrameRead: results => {console.log(results);},
+    onNewCodeRead: (txt, result) => {alert(txt + "Read it");}
+});
+scanner.open().catch(ex=>{
+    console.log(ex);
+    alert(ex.message || ex);
+    scanner.close();
+});
